75b76c14f12973958c41a8482d5baf829fee0a30
Gustav Johansson <gustav@neer.se>
2023-01-06 21:36:35 +0100

    Fixed version.

--- build-image.sh
+++ build-image.sh
@@ -3,14 +3,13 @@
 set -o errexit
 
 wget http://dl-cdn.alpinelinux.org/alpine/latest-stable/releases/x86_64/latest-releases.yaml
-F=alpine-v.$(cat latest-releases.yaml | grep -m 1 "version:" | awk '{print $2}')
-#F=alpine-virt-image-$(date +%Y-%m-%d-%H%M)
+F_ver=v.$(cat latest-releases.yaml | grep -m 1 "version:" | awk '{print $2}')
+F=alpine-$F_ver
 
 if [ "$CI" = "true" ]
 then
     echo "Running under CI"
-    echo "$F"
-    echo $F > version
+    echo $F_ver > version
 fi
 
 ./alpine-make-vm-image/alpine-make-vm-image --packages "openssh e2fsprogs-extra" --script-chroot --image-format qcow2 $F.qcow2 -- ./setup.sh
