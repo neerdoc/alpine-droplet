7ed0cde61c4dda83ac7a1bf5489b4c7806b6496a
Gustav Johansson <gustav@neer.se>
2023-01-08 21:58:03 +0100

    Fixed eth1.

--- setup.sh
+++ setup.sh
@@ -15,7 +15,7 @@ ln -s networking /etc/init.d/net.eth0
 ln -s networking /etc/init.d/net.eth1
 
 rc-update add net.eth0 default
-rc-update add net.eth1 private
+rc-update add net.eth1 default
 rc-update add net.lo boot
 
 # Create root ssh directory
